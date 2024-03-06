99b33ca4faf50f50c64132c2a69381554cba987b
hayato-miura <miura.miura.515@gmail.com>
2024-03-06 10:40:50 +0900

    CSRF対策とトークンの挙動の確認をしました

--- work/public/index.php
+++ work/public/index.php
@@ -1,8 +1,12 @@
 <?php 
+
+session_start();
 define('DSN','mysql:host=db;dbname=myapp;charset=utf8mb4');
 define('DB_USER','myappuser');
 define('DB_PASS','myapppass');
 define ('SITE_URL','https://'.$_SERVER['HTTP_HOST']);
+
+createToken();
 
 try {
   $pdo = new PDO(
@@ -39,7 +43,22 @@ function h($str){
   return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
+function createToken(){
+  if(!isset($_SESSION['token'])){
+    $_SESSION['token'] =bin2hex(random_bytes(32));
+  }
+}
+function valideToken() {
+  if(
+    empty($_SESSION['token'])||
+    $_SESSION['token']!== filter_input(INPUT_POST,'token')
+  )
+  exit('Invalid post request');
+}
+
+
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
+  valideToken();
   addTodo($pdo);
   header ('Location:'.SITE_URL);
   exit;
@@ -66,6 +85,7 @@ $todos = getTodos($pdo);
 
   <form action="" method="post">
     <input type="text" name='title' placeholder="Type new todo." >
+    <input type="hidden" name="token" value="<?=  h($_SESSION['token']);?>">
   </form>
 
   <ul>
