83e3cf734efaacfd8df6be0ff5a03ebca626db61
hayato-miura <miura.miura.515@gmail.com>
2024-03-08 15:25:18 +0900

    Create Todo.php

--- work/app/config.php
+++ work/app/config.php
@@ -6,7 +6,17 @@ define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
 define('DB_USER', 'myappuser');
 define('DB_PASS', 'myapppass');
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
-require_once(__DIR__.'/Utils.php');
-require_once(__DIR__.'/Token.php');
-require_once(__DIR__.'/Database.php');
-require_once(__DIR__.'/functions.php');
+
+
+spl_autoload_register(function ($class){
+    $fileName = sprintf(__DIR__.'/%s.php',$class);
+    if (file_exists($fileName)) {
+        # code...
+        require($fileName);
+    }else{
+        echo 'File not found; '.$fileName;
+        exit;
+    }
+
+
+});
